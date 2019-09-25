a5cc90d228602b8e334ebc5a671f8b90d9fe4ec2
juan1445 <juanzuleta1445@gmail.com>
2019-09-25 13:58:06 -0500

    Servidor desde cero super claro

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+
+const { config } = require('./config/index');
 // importar las rutas de las plantas
 const plantsApi = require('./routes/plantRoute');
+
+//body-parser
+app.use(express.json());
 
 // enviar la app de express a las rutas de las plantas
 plantsApi(app);
@@ -10,7 +15,7 @@ app.get('/', (req,res) => {
     res.send('hello hello');
 });
 
-app.listen(5700, () => {
+app.listen(config.port, () => {
     console.log(`Escuchando por http://localhost:5700`);
 });
 
--- services/plantService.js
+++ services/plantService.js
@@ -1,13 +1,24 @@
 // hacia donde va la persistencia ( logica de negocio )
-const plantsMocks = require('../utils/mocks/plantsMocks');
+
+// este ya no es necesario porque ya estamos conectando a la base de datos y no a los mocks
+//const plantsMocks = require('../utils/mocks/plantsMocks');
+
+
+const MongoLib = require('../lib/mongo');
+
+
+
 
 class PlantsServices {
     constructor(){
-
+        this.collection = 'plants';
+        this.mongoDB = new MongoLib();
     }
 
+    //tags es para filtrar
     async getPlants( { tags } ){
-        const plants = await Promise.resolve(plantsMocks);
+        const query = tags && {$in: {tags}}
+        const plants = await this.mongoDB.getAll(this.collection, query);
         // si no las encuentra envieme un array vacio
         return plants || [];
     }
